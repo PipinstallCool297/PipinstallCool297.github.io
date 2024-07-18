361ac5b30d72d24a0bded56d4b13301295140878
PipinstallCool297 <navya19mehta@gmail.com>
2024-07-18 15:34:53 -0700

    i deleted some files that i didn't need and updated index.html to match my main file

--- level 4 copy/app.py
+++
@@ -1,4 +0,0 @@
-from pymongo import MongoClient
-import pymongo
-client=MongoClient("mongodb+srv://navya19mehta:_database1_@cluster0.by8g6ao.mongodb.net/?retryWrites=true&w=majority")
-db=client.test
